cb598576115c0439a6b513534e8b1bee6695f3a3
p0w3rmast3r <74818475+p0w3rmast3r@users.noreply.github.com>
2022-02-02 15:17:58 +0100

    Update basic_laender.sql

--- sql/basic_laender.sql
+++ sql/basic_laender.sql
@@ -1,12 +1,17 @@
-DROP TABLE IF EXISTS `basic_laender`;
 CREATE TABLE `basic_laender` (
-  `laender_key` int(3) UNSIGNED ZEROFILL NOT NULL,
-  `laender_name` varchar(255) NOT NULL,
-  `laender_iso` varchar(3) NOT NULL
+  `laender_key` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `laender_code` varchar(2) DEFAULT NULL,
+  `name_en` varchar(255) DEFAULT NULL,
+  `name_de` varchar(255) DEFAULT NULL,
+  `name_es` varchar(255) DEFAULT NULL,
+  `name_fr` varchar(255) DEFAULT NULL,
+  `name_it` varchar(255) DEFAULT NULL,
+  `name_ru` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `basic_laender`
   ADD PRIMARY KEY (`laender_key`);
-
-ALTER TABLE `basic_laender`
-  MODIFY `laender_key` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+  
+  ALTER TABLE `basic_laender`
+  MODIFY `laender_key` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+COMMIT;
